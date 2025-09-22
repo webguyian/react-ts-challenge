f2638b37a8d95ab37f70abc3e90e5e11e2a16744
Ian McPhail <webguyian@gmail.com>
2025-09-22 17:21:14 -0400

    Add Instructions component

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,12 @@
 import './App.css';
+import { Instructions } from './components/Instructions';
 import { MemoryGame } from './components/MemoryGame';
 
 const App = () => {
   return (
     <div className="app">
-      <h1>Memory Game Challenge 🧠</h1>
-      <div className="instructions">
-        <details open>
-          <summary className="instructions-heading">Instructions</summary>
-          <p>
-            Complete the game logic by implementing the{' '}
-            <code>handleCardClick</code> function in <code>MemoryGame.tsx</code>
-            . The game should:
-          </p>
-          <ul>
-            <li>Allow flipping a maximum of 2 cards at a time</li>
-            <li>Check if flipped cards match</li>
-            <li>
-              Track the number of moves (a move is when 2 cards are flipped)
-            </li>
-            <li>Detect when all cards are matched to end the game</li>
-          </ul>
-        </details>
-      </div>
+      <h1>🧠 Memory Game Challenge</h1>
+      <Instructions />
       <MemoryGame />
     </div>
   );
